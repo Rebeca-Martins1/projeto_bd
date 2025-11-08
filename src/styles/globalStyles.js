4d64211cff5fec5058c8dbd51ca3f1dab4476cc7
Rebeca-Martins1 <rebecaguimarae@gmail.com>
2025-11-08 15:08:57 -0300

    Agora tem as pastas para cada um dos usuarios

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,10 +6,13 @@ const myGlobalStyles =  createGlobalStyle `
         margin: 0;
         padding:0;
         box-sizing: border-box;
+        font-family: "Noto Sans", sans-serif;
     }
 
     body {
-        background-color: black;
+        min-height: 100vh;
+        width:100vh;
+        background: linear-gradient(180deg, #000000ff 0%, rgba(27, 26, 121, 0.8) 100%);
     }
 `
 
