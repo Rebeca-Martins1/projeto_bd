b5939eeaf59d501a5ac9d9ca7848a7840f990a87
Rafael Castro Moreira <rafacastro2004.rc@gmail.com>
2025-11-09 17:46:06 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <MyGlobalStyles />
     <Routes>
-      <Route path="/" element={<HomePaciente />} />
+      <Route path="/" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/homepaciente" element={<HomePaciente/>} />
     </Routes>
