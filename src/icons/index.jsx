47d99c3eaf7838eb363d7bad6f07ffe981327c9c
Bárbara Neri <barbaranerics@gmail.com>
2025-11-10 18:12:37 -0300

    Ícones na página

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -2,3 +2,6 @@ export { default as IconHeartPulse } from "./IconHeartPulse";
 export { default as IconCalendarPlus } from "./IconCalendarPlus";
 export { default as IconCalendarCheck } from "./IconCalendarCheck";
 export { default as IconClipboardList } from "./IconClipboardList";
+export { default as IconSettings } from "./IconSettings";
+export { default as IconScissors } from "./IconScissors";
+export { default as IconBed } from "./IconBed";
