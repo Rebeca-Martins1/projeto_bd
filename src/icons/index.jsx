ed3f75d804477155522c4db3f4ece5f317e42956
Bárbara Neri <barbaranerics@gmail.com>
2025-11-10 19:03:57 -0300

    Seção plantão adicionada em Home

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -5,3 +5,4 @@ export { default as IconClipboardList } from "./IconClipboardList";
 export { default as IconSettings } from "./IconSettings";
 export { default as IconScissors } from "./IconScissors";
 export { default as IconBed } from "./IconBed";
+export { default as IconHopital } from "./IconHospital";
