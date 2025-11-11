c73af3acd81754ca83ecd8ecb835ad5a455df6f6
Bárbara Neri <barbaranerics@gmail.com>
2025-11-10 21:48:37 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import OcupacaoSalas from './pages/ConselhoPresidente/OcupacaoSalas/index'
 import OcupacaoLeitos from './pages/ConselhoPresidente/OcupacaoLeitos/index'
 import AtividadeMedica from './pages/ConselhoPresidente/AtividadeMedica/index'
 import HomeEnfermeiro from './pages/Enfermeiros/Home/index'
-import Platao from './pages/Enfermeiros/Plantao/index'
+import Plantao from './pages/Enfermeiros/Plantao/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -32,7 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/agendarcirurgia" element={<AgendarCirurgia/>} />
 
       <Route path="/homeenfermeiro" element={<HomeEnfermeiro/>} />
-      <Route path="/platao" element={<Plantao/>} />
+      <Route path="/plantao" element={<Plantao/>} />
 
       <Route path="/homeadm" element={<HomeAdm/>} />
       <Route path="/cadastro_medico_paciente" element={<CadastroProfissional/>} />
