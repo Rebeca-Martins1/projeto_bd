8e97b956a30611fc8b9f6d56d166c50b9622baf6
Bárbara Neri <barbaranerics@gmail.com>
2025-12-08 19:29:05 -0300

    ajsutes

--- src/pages/Enfermeiro/Home/index.jsx
+++ src/pages/Enfermeiro/Home/index.jsx
@@ -1,65 +1,86 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; 
 import * as S from "./styles";
 import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
 
 import { IconSettings, IconScissors, IconBed, IconHospital } from "../../../icons";
 
-function ActionCard({ icon, title, description, buttonText }) {
-  return (
-    <S.ActionCardContainer>
-      <S.ActionCardIcon>{React.createElement(icon)}</S.ActionCardIcon>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <S.ActionCardButton>{buttonText}</S.ActionCardButton>
-    </S.ActionCardContainer>
-  );
+function ActionCard({ icon, title, description, buttonText, onClick }) {
+    return (
+        // Aplicamos onClick ao container inteiro para uma área de clique maior
+        <S.ActionCardContainer onClick={onClick}>
+            <S.ActionCardIcon>{React.createElement(icon)}</S.ActionCardIcon>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            {/* O botão também deve ter a função para garantir o comportamento correto */}
+            <S.ActionCardButton onClick={onClick}>{buttonText}</S.ActionCardButton>
+        </S.ActionCardContainer>
+    );
 }
 
 export default function HomeEnfermeiro() {
-  return (
-    <>
-      <S.GlobalStyles />
-      <S.EnfermeiroPortalContainer>
-        <Header />
+    // 3. Inicializar o hook de navegação
+    const navigate = useNavigate(); 
 
-        <S.MainContent>
-          <S.WelcomeMessage>
-            <h1>BEM-VINDO, [NOME DO ENFERMEIRO]</h1>
-            <p>Portal do Enfermeiro</p>
-          </S.WelcomeMessage>
+    return (
+        <>
+            <S.GlobalStyles />
+            <S.EnfermeiroPortalContainer>
+                <Header />
 
-          <S.CardGrid>
-            <ActionCard 
-              icon={IconSettings}
-              title="EDITAR PERFIL"
-              description="Atualize suas informações"
-              buttonText="IR PRA SEÇÃO"
-            />
-            <ActionCard 
-              icon={IconScissors}
-              title="MINHAS CIRURGIAS"
-              description="Visualize suas cirurgias agendadas e passadas"
-              buttonText="IR PRA SEÇÃO"
-            />
-            <ActionCard 
-              icon={IconBed}
-              title="LEITOS SOB MINHA RESPONSABILIDADE"
-              description="Veja suas ocupações e os detalhes dos leitos"
-              buttonText="IR PRA SEÇÃO"
-            />
+                <S.MainContent>
+                    <S.WelcomeMessage>
+                        <h1>BEM-VINDO, [NOME DO ENFERMEIRO]</h1>
+                        <p>Portal do Enfermeiro</p>
+                    </S.WelcomeMessage>
 
-             <ActionCard 
-              icon={IconHospital}
-              title="PLANTÃO"
-              description="Cadastre e veja seu plantão aqui"
-              buttonText="IR PRA SEÇÃO"
-            />
-          </S.CardGrid>
-        </S.MainContent>
+                    <S.CardGrid>
+                        
+                        {/* 1. EDITAR PERFIL */}
+                        <ActionCard 
+                            icon={IconSettings}
+                            title="EDITAR PERFIL"
+                            description="Atualize suas informações"
+                            buttonText="IR PRA SEÇÃO"
+                            // Adicionar a função de navegação
+                            onClick={() => navigate("/perfil-enfermeiro/editar")} 
+                        />
+                        
+                        {/* 2. MINHAS CIRURGIAS (ESCALA) */}
+                        <ActionCard 
+                            icon={IconScissors}
+                            title="MINHA ESCALA" // Renomeado para ser mais claro
+                            description="Visualize seus procedimentos agendados"
+                            buttonText="IR PRA SEÇÃO"
+                            // Adicionar a função de navegação
+                            onClick={() => navigate("/enfermeiro/escala")} 
+                        />
+                        
+                        {/* 3. LEITOS SOB MINHA RESPONSABILIDADE */}
+                        <ActionCard 
+                            icon={IconBed}
+                            title="LEITOS DE RESPONSABILIDADE" // Título mais conciso
+                            description="Veja suas ocupações e os detalhes dos leitos"
+                            buttonText="IR PRA SEÇÃO"
+                            // Adicionar a função de navegação
+                            onClick={() => navigate("/enfermeiro/meus-leitos")} 
+                        />
 
-        <Footer />
-      </S.EnfermeiroPortalContainer>
-    </>
-  );
+                        {/* 4. PLANTÃO */}
+                        <ActionCard 
+                            icon={IconHospital}
+                            title="PLANTÃO"
+                            description="Cadastre e veja seu plantão aqui"
+                            buttonText="IR PRA SEÇÃO"
+                            // Adicionar a função de navegação
+                            onClick={() => navigate("/enfermeiro/plantao")} 
+                        />
+                    </S.CardGrid>
+                </S.MainContent>
+
+                <Footer />
+            </S.EnfermeiroPortalContainer>
+        </>
+    );
 }
